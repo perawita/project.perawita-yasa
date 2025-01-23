c7d4077a5d68dfd79b436a2558eada53c58768af
Ida Bagus Perawita Yasa <perawitayasa@gmail.com>
2025-01-24 03:18:29 +0800

    update file index.js

--- index.js
+++ index.js
@@ -1,54 +1,51 @@
-const mysql = require('mysql2');
+const mysql = require('mysql2/promise');
 const dbConfig = require('./config/dbConfig');
 
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const port = 3000;
 
-// Buat koneksi
-const connection = mysql.createConnection({
+// Buat koneksi pooling
+const pool = mysql.createPool({
   host: dbConfig.host,
   port: dbConfig.port,
   user: dbConfig.user,
   password: dbConfig.password,
   database: dbConfig.database,
+  waitForConnections: true,
+  connectionLimit: 10, // Jumlah koneksi maksimum dalam pool
+  queueLimit: 0,       // Tidak ada batas antrian koneksi
 });
 
-// Cek koneksi
-connection.connect((err) => {
-  if (err) {
-      console.error('Error connecting to database:', err.message);
-      return;
-  }
-  console.log('Connected to the database:', dbConfig.database);
-});
-  
-
+// Endpoint untuk cek koneksi
 app.get('/', (req, res) => {
   res.send('Hello World!')
-})
-
-app.get('/api/v1/get/:tb_name/list-doa', (req, res) =>{
-    const tb_name = req.params.tb_name
-    const query = `SELECT * FROM ${tb_name}`;
-    
-    connection.query(query, (err, results) => {
-      if (err) {
-        console.error('Error executing query:', err.message);
-        return res.status(500).json({
-          success: false,
-          message: 'Internal Server Error',
-          error: err.message,
-        });
-      }
-      res.json({
-        success: true,
-        message: 'Data retrieved successfully',
-        data: results,
-      });
-    });
 });
 
+// Endpoint untuk mendapatkan data
+app.get('/api/v1/get/:tb_name/list-doa', async (req, res) => {
+  const tb_name = req.params.tb_name;
+
+  // Hindari SQL Injection dengan parameter binding
+  const query = `SELECT * FROM ??`;
+  try {
+    const [results] = await pool.query(query, [tb_name]);
+    res.json({
+      success: true,
+      message: 'Data retrieved successfully',
+      data: results,
+    });
+  } catch (err) {
+    console.error('Error executing query:', err.message);
+    res.status(500).json({
+      success: false,
+      message: 'Internal Server Error',
+      error: err.message,
+    });
+  }
+});
+
+// Jalankan server
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
